67f534df7a3fddb03727db2357c4c77f33a148ad
Kidus Minyelshowa <kidusminyelshowa@gmail.com>
2026-07-28 13:47:43 +0300

    feat: add vite-plugin-static-copy for static asset management

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   server: {
     open: true
-  }
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        { src: 'support.js', dest: '' },
+        { src: 'image-slot.js', dest: '' },
+        { src: '_ds', dest: '' },
+        { src: 'assets', dest: '' }
+      ]
+    })
+  ]
 });
